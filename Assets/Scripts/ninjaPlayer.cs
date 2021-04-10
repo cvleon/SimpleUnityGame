e546bfc341b5bd14fcf2cbfeba6e34064781714e
Christian <cvleon@berkeley.edu>
2021-04-10 11:49:44 -0700

    Add Run Script + Basic Animations

---
+++ Assets/Scripts/ninjaPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ninjaPlayer : MonoBehaviour
+{
+    public float speed;
+    Rigidbody2D rb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // Storing player's input
+        float input = Input.GetAxisRaw("Horizontal");
+
+        // Moving player
+        rb.velocity = new Vector2(input * speed, rb.velocity.y);
+
+    }
+}
